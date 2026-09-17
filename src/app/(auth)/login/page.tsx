e2c137555b808404273a1a16dfe9500e8f786466
Maryam Shakeel <maryammalikawan5@gmail.com>
2026-09-17 12:05:26 +0500

    TODO-WEB-APP

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,7 +11,7 @@ import { PasswordInput } from "@/components/auth/password-input";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+export const dynamic = 'force-dynamic';
 export default function LoginPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
